59f4fa7dab8412bd8a88ed0a006a77598fea1091
tbanh <tbanh@LOVEMACHINE>
2024-01-21 10:38:02 -0800

    Add project files.

---
+++ 233N_Lab2_Banhart/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _233N_Lab2_Banhart.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
